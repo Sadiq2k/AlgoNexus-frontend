2c79a4cd43a01b2b25b7914692b92f201c76f181
Sadiq2k <sadiqhussain2k23@gmail.com>
2024-06-08 12:10:09 +0530

    modified getToken function

--- src/app/auth/user-auth.service.ts
+++ src/app/auth/user-auth.service.ts
@@ -5,110 +5,47 @@
   })
   export class UserAuthService {
 
-  //   constructor() {}
+    constructor() {}
 
-  //   public setRoles(roles: any[]) {
-  //     localStorage.setItem('roles', JSON.stringify(roles));
-  //   }
-
-  //   public getRoles(): string[] | null {
-  //     const rolesJson = localStorage?.getItem('roles');
-  //     return rolesJson ? JSON.parse(rolesJson) : null;
-  // }
-
-  //   public setToken(jwtToken: string) {
-  //     localStorage.setItem('jwtToken', jwtToken);
-  //   }
-
-  //   public getToken(): string|null {
-  //     const token = localStorage?.getItem('jwtToken');
-  //     return token || ''; 
-  // }
-  // public setUserId(userId:string){
-  //   localStorage.setItem('userId',JSON.stringify(userId));
-  // }
-  // public getUserId(){
-  //   const userIdJson = localStorage?.getItem('userId');
-  //   return userIdJson ? JSON.parse(userIdJson) : null;
-  // }
-
-  //   public clear() {
-  //     localStorage.clear();
-  //   }
-
-  //   public isLoggedIn() {
-  //     return this.getRoles() && this.getToken();
-  //   }
-
-  //   public hasRole(role: string): boolean {
-  //     const roles = this.getRoles();
-  //     console.log("roles in authguard: ",roles)
-  //     return roles ? roles.includes(role) : false;
-  //   }
-
-
-  constructor() {}
-
-  private isLocalStorageAvailable(): boolean {
-    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
-  }
-
-  public setRoles(roles: any[]) {
-    if (this.isLocalStorageAvailable()) {
+    public setRoles(roles: any[]) {
       localStorage.setItem('roles', JSON.stringify(roles));
     }
-  }
 
-  public getRoles(): string[] | null {
-    if (this.isLocalStorageAvailable()) {
-      const rolesJson = localStorage.getItem('roles');
+    public getRoles(): string[] | null {
+      const rolesJson = localStorage?.getItem('roles');
       return rolesJson ? JSON.parse(rolesJson) : null;
-    }
-    return null;
   }
 
-  public setToken(jwtToken: string) {
-    if (this.isLocalStorageAvailable()) {
+    public setToken(jwtToken: string) {
       localStorage.setItem('jwtToken', jwtToken);
     }
-  }
 
-  public getToken(): string | null {
-    if (this.isLocalStorageAvailable()) {
+    getToken(): string | null {
       const token = localStorage.getItem('jwtToken');
-      return token || null;
+      return token ? token : null;
     }
-    return null;
+    
+
+  public setUserId(userId:string){
+    localStorage.setItem('userId',JSON.stringify(userId));
+  }
+  public getUserId(){
+    const userIdJson = localStorage?.getItem('userId');
+    return userIdJson ? JSON.parse(userIdJson) : null;
   }
 
-  public setUserId(userId: string) {
-    if (this.isLocalStorageAvailable()) {
-      localStorage.setItem('userId', JSON.stringify(userId));
-    }
-  }
-
-  public getUserId(): string | null {
-    if (this.isLocalStorageAvailable()) {
-      const userIdJson = localStorage.getItem('userId');
-      return userIdJson ? JSON.parse(userIdJson) : null;
-    }
-    return null;
-  }
-
-  public clear() {
-    if (this.isLocalStorageAvailable()) {
+    public clear() {
       localStorage.clear();
     }
-  }
 
-  public isLoggedIn(): boolean {
-    return !!this.getRoles() && !!this.getToken();
-  }
+    public isLoggedIn() {
+      return this.getRoles() && this.getToken();
+    }
 
-  public hasRole(role: string): boolean {
-    const roles = this.getRoles();
-    console.log("roles in authguard: ", roles);
-    return roles ? roles.includes(role) : false;
-  }
+    public hasRole(role: string): boolean {
+      const roles = this.getRoles();
+      console.log("roles in authguard: ",roles)
+      return roles ? roles.includes(role) : false;
+    }
     
   }
